14c6c10235e64d2b88bfc2000f1c5053a628ecdf
Surya <suriya.mudaliar@gmail.com>
2021-08-30 18:26:57 +0530

    Js file Location changed and titles added for Html pages

--- assets/js/list-movies.js
+++ assets/js/list-movies.js
@@ -18,7 +18,7 @@ function formMovieTableData(movies){
     let content = "";
     let i =1;
     for(let movieObj of movies){
-        content += `<tr><td>${i++}</td><td>${movieObj.title}</td><td>${movieObj.language}</td><td><img src="images/${movieObj.imageUrl}" width="50px" height=50px"></td><td><a href="edit-movie.html?id=${movieObj.id}">Edit</a> </td></tr>`;
+        content += `<tr><td>${i++}</td><td>${movieObj.title}</td><td>${movieObj.language}</td><td><img src="images/${movieObj.imageUrl}" width="50px" height=50px"></td><td><a href="edit-movie.html?id=${movieObj.id}">Edit</a> </td><td><button type="button" onclick="deleteMovie(${movieObj.id})">Delete</button></td></tr>`;
     }
     console.log(content);
     document.querySelector("#movies").innerHTML=content;
